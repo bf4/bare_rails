0e3b57827c7870f61195a6821c4deb1f61f39079
Benjamin Fleischer <github@benjaminfleischer.com>
2013-12-03 17:05:55 -0600

    Init very bare-bones rails app
    rails-api new bare_rails --skip-action-mailer --skip-active-record
    --skip-test-unit --skip-javascript --skip-sprockets

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'actionpack',    '~> 4.0'
+gem 'activesupport', '~> 4.0'
+gem 'railties',      '~> 4.0'
+gem 'sprockets-rails', '~> 2.0.0'
+gem 'bundler',         ['< 2.0', '>= 1.3.0']
+
+gem 'rails-api',    '~> 0.1'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4
+# requires secret_key_base or secret_toke to be defined, otherwise an
+# error is raised.
+# Can be safely removed in a rails3 api-only application.
+BareRails::Application.config.secret_key_base = ENV['SECRET_TOKEN'] or fail "ENV does not have SECRET TOKEN"
